bd252f8783dc0b0a916ece033797b268d251ef4b
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-11-10 19:34:28 -0800

    Initial pseudocode for diagonal difference

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -5,4 +5,12 @@ function main() {
        a[a_i] = readLine().split(' ');
        a[a_i] = a[a_i].map(Number);
    	}
+   	// instantiate firstDiagonal counter, secondDiagonal counter
+   	// iterate through numbers up to n dimension counter
+   		// firstDiagonal += a[i][i]
+   		// secondDiagonal += a[i][n-[i+1]]
+   	// check if firstDiagonal larger
+   		// console.log difference
+   	// check if secondDiagonal larger
+   		// console.log difference
 }
