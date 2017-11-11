4e90bf2cefba8a65be4ee8a56d1a8ae416dfe8a7
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-11-10 19:16:44 -0800

    Implement pseudocode for compare the triplets

--- compareTheTriplets.js
+++ compareTheTriplets.js
@@ -14,5 +14,24 @@ function solve(a0, a1, a2, b0, b1, b2){
         // increment aliceScore
     // return 
         // aliceScore space bobScore string
-
+    var aliceScore=0, bobScore=0;
+    if(a0<b0){
+        bobScore++;
     }
+    if(a0>b0){
+        aliceScore++;
+    }
+    if(a1<b1){
+        bobScore++;
+    }
+    if(a1>b1){
+        aliceScore++;
+    }
+    if(a2<b2){
+        bobScore++;
+    }
+    if(a2>b2){
+        aliceScore++;
+    }
+    console.log(aliceScore + " " + bobScore);
+}
