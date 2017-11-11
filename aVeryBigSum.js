81c25d09ed42709cba440c2bf8392370b04e448a
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-11-10 19:29:33 -0800

    Implementation of pseudocode for a very big sum

--- aVeryBigSum.js
+++ aVeryBigSum.js
@@ -4,4 +4,9 @@ function aVeryBigSum(n, ar) {
     // loop through array
         // increment counter by each item
     // return counter
+    var counter = 0;
+    for(var i=0; i<ar.length;i++){
+        counter+=ar[i];
+    }
+    return counter;
 }
