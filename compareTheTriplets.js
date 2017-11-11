136d74f2abf4a2843a2222cfb668d2fd0ed1062c
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-11-10 19:16:08 -0800

    Initial Compare the Triplets

---
+++ compareTheTriplets.js
@@ -0,0 +1,18 @@
+function solve(a0, a1, a2, b0, b1, b2){
+    // instantiate bobScore, aliceScore equal to 0
+    // if a0<b0
+        // increment bobScore
+    // if a0>b0
+        // increment aliceScore
+    // if a1<b1
+        // increment bobScore
+    // if a1>b1
+        // increment aliceScore
+    // if a2<b2
+        // increment bobScore
+    // if a2>b2
+        // increment aliceScore
+    // return 
+        // aliceScore space bobScore string
+
+    }
