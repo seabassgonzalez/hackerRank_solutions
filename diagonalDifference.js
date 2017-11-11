e689ba3362d25c4714461a2d3a71de4c96031495
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-11-10 19:30:50 -0800

    Initial diagonal difference

---
+++ diagonalDifference.js
@@ -0,0 +1,8 @@
+function main() {
+    var n = parseInt(readLine());
+    var a = [];
+    for(a_i = 0; a_i < n; a_i++){
+       a[a_i] = readLine().split(' ');
+       a[a_i] = a[a_i].map(Number);
+   	}
+}
