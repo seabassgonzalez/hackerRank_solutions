f0752abb68ecf37705fc140f209c1fb3544daa7c
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-11-10 19:41:35 -0800

    Initial pseudocode for plus minus

--- plusMinus.js
+++ plusMinus.js
@@ -3,4 +3,16 @@ function main() {
     arr = readLine().split(' ');
     arr = arr.map(Number);
 
+    // instantiate positive, negative, and zeroes counters
+    // loop through the array
+    	// if array item > 0
+    		// increment positive
+    	// if array item < 0
+    		// increment negative
+    	// if array item == 0
+    		// increment zeroes
+    // console.log positive / n
+    // console.log negative / n
+    // console.log zeroes / n 
+    
 }
