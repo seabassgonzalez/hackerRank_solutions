73be9b3327c40e90ee4d190b9933b8e2436bef1b
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-11-10 19:17:41 -0800

    Initial pseudocode for a very big sum

---
+++ aVeryBigSum.js
@@ -0,0 +1,7 @@
+function aVeryBigSum(n, ar) {
+    // Complete this function
+    // instantiate counter
+    // loop through array
+        // increment counter by each item
+    // return counter
+}
