e9d5376c1b9faa55ac59eae4d87203cc7e4eab97
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-11-10 19:38:17 -0800

    Initial implementation of pseudocode for diagonal difference

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -13,4 +13,15 @@ function main() {
    		// console.log difference
    	// check if secondDiagonal larger
    		// console.log difference
+   	
+   	var firstDiagonal=0, secondDiagonal=0;
+   	for(var i=0;i<n;i++){
+   		firstDiagonal += a[i][i];
+   		secondDiagonal +=a[i][n-[i+1]];
+   	}
+   	if(firstDiagonal>secondDiagonal){
+   		console.log(firstDiagonal - secondDiagonal);
+   	}else{
+   		console.log(secondDiagonal - firstDiagonal);
+   	}
 }
